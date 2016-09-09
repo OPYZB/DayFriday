c768cbc72240bb2cc09c0afc6e13bcdfdc1581c4
CHERISH <qq@simpleli.local>
2016-09-09 17:56:06 +0800

    Initial Commit

---
+++ DayFriday/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DayFriday
+//
+//  Created by qq on 16/9/9.
+//  Copyright © 2016年 yuncheda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
