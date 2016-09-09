e8e6ea94ca2bb7c5f14fec11d16980598e1931fb
CHERISH <qq@simpleli.local>
2016-09-09 18:01:38 +0800

    新建两个文件

---
+++ DayFriday/asasasViewController.h
@@ -0,0 +1,13 @@
+//
+//  asasasViewController.h
+//  DayFriday
+//
+//  Created by qq on 16/9/9.
+//  Copyright © 2016年 yuncheda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface asasasViewController : UIViewController
+
+@end
---
+++ DayFriday/asasasViewController.m
@@ -0,0 +1,13 @@
+//
+//  asasasViewController.m
+//  DayFriday
+//
+//  Created by qq on 16/9/9.
+//  Copyright © 2016年 yuncheda. All rights reserved.
+//
+
+#import "asasasViewController.h"
+
+@implementation asasasViewController
+
+@end
